930c7ef1a3d70ed35e7372f0ed5bfc51999adcf1
kaiserkernel <kaiserkernel84@gmail.com>
2025-02-19 06:13:43 -0800

    feat: fetch chart data two case

---
+++ src/constant/collegeList.ts
@@ -0,0 +1,26 @@
+export const collegeList = [
+    {
+        name: "Math",
+        value: "College of Science and Mathematics Education"
+    },
+    {
+        name: "Social",
+        value: "College of Social Sciences Education"
+    },
+    {
+        name: "Language",
+        value: "College of Languages Education"
+    },
+    {
+        name: "Education",
+        value: "College of Educational Sciences"
+    },
+    {
+        name: "Sport",
+        value: "Institute of Physical Education and Sport"
+    },
+    {
+        name: "Business",
+        value: "College of Business, Technology and Vocational Education"
+    }
+]
--- src/routes/chart/index.ts
+++ src/routes/chart/index.ts
@@ -1,3 +1,4 @@
+import { collegeList } from "@/constant/collegeList";
 import { Announcement } from "@/models/announcementModel";
 import { Application } from "@/models/applicationModel";
 import { Request, Response, Router } from "express";
@@ -13,8 +14,9 @@ router.post("/", async (req: Request, res: Response) => {
             res.status(400).json({success: false, message: "Please send requirement"});
         }
 
+        let result;
         if (xAxios === 'college' && yAxios === 'budget') {
-            const result = await Application.aggregate([
+            result = await Application.aggregate([
                 {
                     $match: { finance: 'approved' }
                 },
@@ -23,11 +25,59 @@ router.post("/", async (req: Request, res: Response) => {
                         _id: "$college",
                         totalBudget: { $sum: "$budget" }
                     }
+                },
+                {
+                    $project: {
+                        content: "$_id",
+                        totalBudget: 1,
+                        _id: 0
+                    }
                 }
             ]);
-            console.log(result, 'result')
-            res.status(200).json({success: true, data: result});
+
+            const collegeListMap = new Map<string, string>();
+            collegeList.forEach(college => collegeListMap.set(college.value, college.name));
+            
+            result = result.map(log => {
+                const title = collegeListMap.get(log.content);
+                return {
+                    ...log,
+                    title
+                }
+            })
+        } else if (xAxios === 'announcement' && yAxios === 'budget') {
+            result = await Application.aggregate([
+                {
+                    $match: { finance: 'approved' }
+                },
+                {
+                    $group: {
+                        _id: "$announcement",
+                        totalBudget: { $sum: "$budget" }
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "announcements",
+                        localField: "_id",
+                        foreignField: "_id",
+                        as: "announcementData"
+                    }
+                },
+                {
+                    $unwind: "$announcementData" // convert array to object
+                },
+                {
+                    $project: {
+                        _id: 0,
+                        totalBudget: 1,
+                        title: "$announcementData.title"
+                    }
+                }
+            ])
         }
+        
+        res.status(200).json({success: true, data: result});
     } catch (error) {
         console.log(error, 'fetch chart data error');
         res.status(500).json({ success: false, message: "Internal Server Error" });
